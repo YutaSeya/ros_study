491e10fda2935344b2d9668977e2f8d45dcec809
YutaSeya <bp17069@shibaura-it.ac.jp>
2019-10-07 05:58:50 -0700

    add ros lecture 13

---
+++ catkin_ws/devel/share/basic_lecture/cmake/basic_lecture-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(basic_lecture_MESSAGE_FILES "/home/sora/ros_study/catkin_ws/src/ros_lecture/basic_lecture/msg/cmsg.msg")
+set(basic_lecture_SERVICE_FILES "")
---
+++ catkin_ws/devel/share/basic_lecture/cmake/basic_lecture-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(basic_lecture_MSG_INCLUDE_DIRS "/home/sora/ros_study/catkin_ws/src/ros_lecture/basic_lecture/msg")
+set(basic_lecture_MSG_DEPENDENCIES std_msgs)
